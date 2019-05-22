9d482d6fce6152d0c55e4ac17a5021255646218e
Brianna DelValle <briannadelvalle@gmail.com>
2019-05-22 14:51:02 -0600

    Interpolate object info into card template literal

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var titleInput = document.querySelector('#title-input');
 var bodyInput = document.querySelector('#body-input');
 var saveButton = document.querySelector('#save-button');
+var cardSection = document.querySelector('#card-section')
 var ideasArray = [];
+
+localStorage.setItem('ideas array', JSON.stringify(ideasArray));
 
 saveButton.addEventListener('click', saveFunction)
 
@@ -44,27 +47,35 @@ titleInput.value === '' || bodyInput.value === '' ? saveButton.disabled = true: 
 //Populating Cards//
 
 //QuerySelect the card table
-var cardSection = document.querySelector('#card-section')
+
 
 
 function abraCadabra(){
-// Declare Variable for ideaCard
-var ideaCard =  `<div class="card-top">
+
+for (var i = 0; i < ideasArray.length; i++) {
+  console.log(ideasArray[i].name);
+
+
+
+var ideaCard =  `<div class="card-top" data-id="${ideasArray[i].id}">
           <button class="star-button"><img src="images/star.svg"></button>
           <button class="delete-button"><img src="images/delete.svg"></button>
         </div>
-        <h3>Idea title</h3>
-        <p class="idea-body">Stuff things, other things and some more stuff, things stuffed with stuff and stuff made up of things that have stuff in them built by things that make stuff that are made from stuff become things</p>
+        <h3>${ideasArray[i].title}</h3>
+        <p class="idea-body">${ideasArray[i].body}</p>
         <div class="card-bottom">
           <button class="upvote-button"><img src="images/upvote.svg" class="upvote-svg"></button>
           <p class="quality-label">Quality:<span>Swill</span></p>
           <button class="downvote-button"><img src="images/downvote.svg"></button>
         </div>`
-  //Interpolate the id, title, and body based
 
+        cardSection.insertAdjacentHTML('afterbegin', ideaCard);
 
-
-
-
+  }
 }
+
+
+
+
+abraCadabra()
 
