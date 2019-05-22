0bebf8fc8705c650c614cfd5b318f7c5750755a3
Chris Lane <pankokrumbs@gmail.com>
2019-05-22 09:02:32 -0600

    Adds refactored disable save button and pseudocode for card population

--- main.js
+++ main.js
@@ -56,18 +56,35 @@ titleInput.addEventListener('keyup', enableSaveButton)
 bodyInput.addEventListener('keyup', enableSaveButton)
 
 function enableSaveButton() {
-    var inputFields =[
-    titleInput.value,
-    bodyInput.value
-    ];
-    for(var i = 0; i < inputFields.length; i++){
-        if (inputFields[i].length < 1) {
-            saveButton.disabled = true;
-            return;
-        } else {
-            saveButton.disabled = false;
-        };
-    };
-};
+titleInput.value === '' || bodyInput.value === '' ? saveButton.disabled = true: saveButton.disabled = false;  
+}
 
+
+
+//Populating Cards//
+
+//QuerySelect the card table
+var cardSection = document.querySelector('#card-section')
+
+
+function abraCadabra(){
+// Declare Variable for ideaCard
+var ideaCard =  `<div class="card-top">
+          <button class="star-button"><img src="images/star.svg"></button>
+          <button class="delete-button"><img src="images/delete.svg"></button>
+        </div>
+        <h3>Idea title</h3>
+        <p class="idea-body">Stuff things, other things and some more stuff, things stuffed with stuff and stuff made up of things that have stuff in them built by things that make stuff that are made from stuff become things</p>
+        <div class="card-bottom">
+          <button class="upvote-button"><img src="images/upvote.svg" class="upvote-svg"></button>
+          <p class="quality-label">Quality:<span>Swill</span></p>
+          <button class="downvote-button"><img src="images/downvote.svg"></button>
+        </div>`
+  //Interpolate the id, title, and body based
+
+
+
+
+
+}
 
